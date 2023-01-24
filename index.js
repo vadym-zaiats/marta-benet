e999bf6ebb3b810dba4d110cba9fdbc7a97f5412
Vadim Zayats <vadyk.za@gmail.com>
2023-01-25 01:05:26 +0530

    slider ready

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 "use strict";
 let offset = 0;
-let parent = document.querySelector(".novelties_arrows");
+let parent = document.querySelector(".event-listener");
 let sliderLine = document.querySelector(".novelties_list");
 
 parent.addEventListener("click", (e) => {
   if (e.target.classList.contains("button-next")) {
     offset += 359;
-    if (offset > 1436) {
+    if (offset > 2154) {
       offset = 0;
     }
     sliderLine.style.left = -offset + "px";
   }
-});
-parent.addEventListener("click", (e) => {
   if (e.target.classList.contains("button-prev")) {
     offset -= 359;
     if (offset < 0) {
-      offset = 1436;
+      offset = 2154;
     }
     sliderLine.style.left = -offset + "px";
+  }
+  if (e.target.classList.contains("novelties_viewall")) {
+    document
+      .querySelector(".novelties_list")
+      .classList.replace("novelties_list", "novelties_list-all");
+    e.target.classList.toggle("hidden");
+    document.querySelector(".novelties_viewless").classList.toggle("hidden");
+    document.querySelector(".novelties_arrows").classList.toggle("hidden");
+  }
+  if (e.target.classList.contains("novelties_viewless")) {
+    document
+      .querySelector(".novelties_list-all")
+      .classList.replace("novelties_list-all", "novelties_list");
+    e.target.classList.toggle("hidden");
+    document.querySelector(".novelties_viewall").classList.toggle("hidden");
+    document.querySelector(".novelties_arrows").classList.toggle("hidden");
   }
 });
